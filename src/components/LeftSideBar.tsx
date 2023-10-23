e12094dbb8a085d1d6ef077a529c7cdb7e97965b
soumyarajbag <soumyarajbag@gmail.com>
2023-10-23 18:29:12 +0530

    Homepage is in process

---
+++ src/components/LeftSideBar.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
+import { HiEnvelope } from "react-icons/hi2";
+
+const NAVIGATION_ITEMS = [
+    {
+      title: "Twitter",
+      icon: BsTwitter,
+    },
+    {
+      title: "Home",
+      icon: BiHomeCircle,
+    },
+    {
+      title: "Explore",
+      icon: HiOutlineHashtag,
+    },
+    {
+      title: "Notifications",
+      icon: BsBell,
+    },
+    {
+      title: "Messages",
+      icon: BsEnvelope,
+    },
+    {
+      title: "Bookmarks",
+      icon: BsBookmark,
+    },
+    {
+      title: "Profile",
+      icon: BiUser,
+    },
+  ];
+const LeftSideBar = () => {
+  return (
+    <section className="w-[275px]  fixed flex flex-col items-stretch h-screen  ">
+    <div className="flex flex-col items-stretch  space-y-4  h-full mt-4">
+      {NAVIGATION_ITEMS.map((item, index) => (
+        <Link
+          href={`/${item.title.toLowerCase()}`}
+          className=" flex text-2xl justify-start w-fit space-x-6 items-center tansition duration-200 hover:bg-white/50 rounded-3xl px-6 py-2"
+          key={index}
+        >
+          <div>
+            <item.icon />
+          </div>
+          <div>{item.title != "Twitter" && <div>{item.title}</div>}</div>
+        </Link>
+      ))}
+      <button className="w-full rounded-full bg-primary m-4   p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+        Tweet
+      </button>
+    </div>
+
+    <button className=" rounded-full w-full hover:bg-white/10 flex justify-between items-center space-x-2 bg-transparent m-4   p-4  text-center hover:bg-opacity-70 transition duration-200">
+      <div className="flex items-center space-x-2">
+        <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+        <div className="text-sm text-left">
+          <div className="font-semibold">Soumyaraj Bag</div>
+          <div className="">@soumyarajbag</div>
+        </div>
+      </div>
+      <div>
+        <BsThreeDots />
+      </div>
+    </button>
+  </section>
+  )
+}
+
+export default LeftSideBar
---
+++ src/components/MainComponents.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { BsDot , BsChat , BsThreeDots } from "react-icons/bs";
+import { AiOutlineRetweet, AiOutlineHeart } from "react-icons/ai";
+import { IoStatsChart, IoShareOutline } from "react-icons/io5";
+const MainComponents = () => {
+  return (
+    <main className="ml-[300px] mx-2  flex w-[650px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+    <h1 className="text-xl font-bold  p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
+    <div className="border-t-[0.5px] border-b-[0.5px] px-4 border-gray-600  py-4 relative flex items-stretch space-x-2">
+      <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
+      <div className="flex flex-col w-full h-full">
+        <input
+          type="text"
+          className="w-full h-full font-semibold text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 border-none outline-none"
+          placeholder="What's Happening ?"
+        />
+
+        <div className="w-full justify-between items-center flex">
+          <div></div>
+          <div className="w-full max-w-[100px]">
+            <button className=" rounded-full bg-primary py-2 w-full  px-4 text-lg text-center hover:bg-opacity-70  transition duration-200 font-bold">
+              Tweet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="flex flex-col">
+      {Array.from({ length: 5 }).map((_, index) => (
+        <div
+          key={index}
+          className="border-b-[0.5px] border-gray-600 py-4 px-6  flex space-x-4"
+        >
+          <div>
+            <div className="w-10 h-10 bg-slate-200 rounded-full" />
+          </div>
+          <div className="flex flex-col">
+            <div className="flex items-center my-1 w-full">
+             <div className="flex items-center space-x-1 w-full">
+             <div className="font-bold">Soumyaraj Bag</div>
+              <div className="text-gray-500">@soumyarajbag</div>
+              <div className="text-gray-500">
+                <BsDot />
+              </div>
+              <div className="text-gray-500">1 hour ago</div>
+              
+             </div>
+             <div>
+                <BsThreeDots />
+              </div>
+            </div>
+            <div className="text-white text-base">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Mollitia est reiciendis, totam fugiat suscipit atque eaque
+              reprehenderit eius placeat rem facilis molestias, at optio
+              cum incidunt exercitationem. Adipisci consequatur dolorum
+              vitae, iste, porro fugit similique cum ad perferendis
+              cupiditate excepturi voluptas quod molestiae voluptatum
+              inventore dolorem soluta! Alias quos omnis repellat nulla,
+              illum ut minima facilis maiores ad unde nesciunt beatae
+              rerum sint dolores necessitatibus accusamus quaerat hic
+              laudantium ducimus labore ipsam? Eius nemo recusandae quasi
+              pariatur porro delectus amet tempore. Corrupti reprehenderit
+              repellat deleniti veritatis delectus sint placeat nihil,
+              ipsum molestiae quae alias tempora eligendi officia rerum
+              minus quidem.
+            </div>
+            <div className="bg-slate-400 aspect-square w-full h-96 rounded-xl mt-2">
+
+            </div>
+
+            <div className="flex items-center justify-start space-x-20 mt-2 w-full">
+              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer"><BsChat /></div>
+              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer"><AiOutlineRetweet /></div>
+              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer"><AiOutlineHeart /></div>
+              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer"><IoStatsChart /></div>
+              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer"><IoShareOutline /></div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </main>
+  )
+}
+
+export default MainComponents
